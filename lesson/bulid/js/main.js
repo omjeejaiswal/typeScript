63425163c3ba93bae7ccb2d831bede3ba54ef0aa
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-29 23:23:27 +0530

    .

---
+++ lesson/bulid/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+let username = 'Omjee';
+console.log(username);
+// reason for the first error was same username definded in js and ts.
+// let a  = 12
+// let b = '6'
+// let c = 2
+// console.log(a/b) 
+// // in js it does not check that b is string and automaitcally answer 12/6.
+// console.log(c * b)
+// // in js it does not check that b is string and automaitcally answer 2 * 6 = 12.
+let a = 12;
+let b = 6;
+// let b: string ='6'
+let c = 2;
+console.log(a / b);
+console.log(c * b);
