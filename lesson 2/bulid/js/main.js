04caffbf35e1b974e62d0719b1a202cf60e02529
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-30 00:01:18 +0530

    .

---
+++ lesson 2/bulid/js/main.js
@@ -0,0 +1,33 @@
+"use strict";
+// let myName: string = 'Omjee'
+// or 
+// let myName = 'Omjee'
+// let myName: string;
+// myName = 'Omjee'
+// myName= 'Jaiswal'
+// let myName: string ='Omjee'
+// myName = 'Jaiswal'
+let myName = 'Omjee';
+let meaningOfLife;
+let isLoading;
+let album;
+let mergetype;
+myName = 'Jaiswal';
+meaningOfLife = 22;
+isLoading = true;
+album = 'omjee jasiwal';
+album = 2003;
+album = '6';
+album = 'sureshjaiswal808@gmail.com';
+// const sum = (a,b) => {
+//     return a + b
+// }
+const sum = (a, b) => {
+    return a + b;
+};
+const sum1 = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
