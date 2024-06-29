e02e42228255bb3bd32f4e1b5385d20f7f90e0e3
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-30 00:01:30 +0530

    main

---
+++ lesson 2/src/main.ts
@@ -0,0 +1,44 @@
+// let myName: string = 'Omjee'
+// or 
+// let myName = 'Omjee'
+
+// let myName: string;
+
+// myName = 'Omjee'
+// myName= 'Jaiswal'
+
+// let myName: string ='Omjee'
+
+// myName = 'Jaiswal'
+
+let myName: string = 'Omjee'
+let meaningOfLife: number
+let isLoading: boolean
+let album: any
+let mergetype: string | number
+
+myName = 'Jaiswal'
+meaningOfLife= 22
+isLoading = true
+album = 'omjee jasiwal'
+album = 2003
+album = '6' 
+album = 'sureshjaiswal808@gmail.com'
+
+// const sum = (a,b) => {
+//     return a + b
+// }
+
+const sum = (a:number, b: number) => {
+    return a + b
+}
+
+const sum1 = (a:number , b:string) => { 
+    return a + b
+} 
+
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp = /\w+/g
