1f110cba71e17d17e6e35e7c346ed33ee8bac49a
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-30 12:27:23 +0530

    .

--- lesson 4_function/src/main.ts
+++ lesson 4_function/src/main.ts
@@ -17,3 +17,19 @@ let myName: 'Omjee'
 
 let userName : 'Omjee' | 'Jasiwal' | 'Amy'
 userName = 'Amy'
+
+
+// functions
+const add = (a:number,b:number): number => {
+    return a + b
+}
+
+const logMsg = (message: any): void => {
+    console.log(message)
+}
+
+
+logMsg('Hello!')
+logMsg(add(2,3))
+
+
