cde3a3410cde728269c574f6a0a17fbb7e4995d2
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-30 12:07:28 +0530

    Aliases

--- lesson 4_function/src/main.ts
+++ lesson 4_function/src/main.ts
@@ -1,7 +1,12 @@
+// type Aliases
+type stringOrNumber = string | number
 
+type stringOrNumberArray = (string | number)[]
 
 type Guitarist = {
     name?: string,
     active: boolean,
     albums: (string | number)[]
 }
+
+type UserId = stringOrNumber
