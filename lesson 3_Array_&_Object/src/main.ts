4482f6dbdc1f439fbb471fb77b24182f411bd988
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-30 11:53:32 +0530

    object

--- lesson 3_Array_&_Object/src/main.ts
+++ lesson 3_Array_&_Object/src/main.ts
@@ -34,6 +34,93 @@ myTuple[1] = 42
 
 
 
+// object
+let myObj: object
+myObj = []
+console.log(typeof myObj)
+myObj = bands
+myObj = {}
+
+const exampleObj = {
+    prop1: 'Omjee',
+    prop2: true
+}
+
+exampleObj.prop1 = 'Omjee'
+
+// type Guitarist = {
+//     name: string,
+//     active: boolean,
+//     active2?: boolean, // active2?: means this is optional 
+//     albums: (String | number)[]
+// }
+
+interface Guitarist {
+    name: string,
+    active: boolean,
+    active2?: boolean, // active2?: means this is optional 
+    albums: (String | number)[]
+}
+let evh: Guitarist = {
+    name: 'jasiwal',
+    active: false,
+    albums: [19984, 5150, 'OU452']
+}
+
+
+let JP: Guitarist = {
+    name: 'omjee JAiswal',
+    active: true,
+    albums: [1, 2, 'O']
+}
+
+// evh = JP
+
+
+// const greetGuitarist = (guitarist: Guitarist) => {
+//     return `hello ${guitarist.name}!`
+// }
+
+// const greetGuitarist = (guitarist: Guitarist) => {
+//     return `hello ${guitarist.name?.toUpperCase()}! using toUpperCase`
+// }
+
+const greetGuitarist = (guitarist: Guitarist) => {
+    if(greetGuitarist.name) {
+        return `hello ${guitarist.name?.toUpperCase()}! using toUpperCase`
+    }
+    return 'Hello!'
+    
+}
+
+const greetGuitarist1 = (guitarist: Guitarist) => {
+    return `hello ${guitarist.name}! `
+}
+
+
+console.log(greetGuitarist(JP))
+console.log(greetGuitarist1(evh))
+
+
+
+// Enums
+// "unlike most TypeScript features,Enums are not a
+//  type-level addition to js but something
+//  added to the language and runtime."
+
+enum Grade {
+    U = 1,
+    D,
+    C,
+    B,
+    A,
+}
+
+console.log(Grade.U)
+
+
+
+
 
 
 
