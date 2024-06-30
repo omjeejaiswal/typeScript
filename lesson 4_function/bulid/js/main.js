652ebfd6a9c7476d7b2f65a2c2f9d3db525a5d9d
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-06-30 12:27:17 +0530

    .

--- lesson 4_function/bulid/js/main.js
+++ lesson 4_function/bulid/js/main.js
@@ -4,3 +4,12 @@ let myName;
 // myName = 'jaiswal'
 let userName;
 userName = 'Amy';
+// functions
+const add = (a, b) => {
+    return a + b;
+};
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg('Hello!');
+logMsg(add(2, 3));
