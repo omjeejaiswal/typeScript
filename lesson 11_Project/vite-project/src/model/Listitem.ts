383f57dab36e4bcadacb576fae5620962c974ee1
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-04 21:04:33 +0530

    ListItem

--- lesson 11_Project/vite-project/src/model/Listitem.ts
+++ lesson 11_Project/vite-project/src/model/Listitem.ts
@@ -4,7 +4,7 @@ export interface Item {
     checked: boolean,
 }
 
-export default class Listitem implements Item {
+export default class listItems implements Item {
     constructor(
         private _id: string ='',
         private _item: string = '',
