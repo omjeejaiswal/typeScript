27b1429500689f1369cb1e1402599cfc466cda27
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-05 00:48:29 +0530

    heading

--- lesson 12/src/components/Heading.tsx
+++ lesson 12/src/components/Heading.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from "react"
 
 type HeadingProps = {title: string}
 
-const Heading = ({title}: HeadingProps) => {
+const Heading = ({title}: HeadingProps):ReactElement => {
     return <h1>{title}</h1>
 }
 export default Heading
