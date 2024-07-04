0e67cdc4e87808a93a5cd6945349abfef78fe9a0
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-04 21:27:09 +0530

    main

--- lesson 11_Project/vite-project/src/main.ts
+++ lesson 11_Project/vite-project/src/main.ts
@@ -1,1 +1,38 @@
 import './css/style.css'
+import FullList from './model/FullList'
+import listItems from './model/Listitem'
+import ListTemplate from './templates/ListTemplate'
+
+
+const initApp =(): void => {
+  const fullList = FullList.instance
+  const templates = ListTemplate.instance
+
+  const itemEntryForm = document.getElementById("itemEntryForm") as HTMLFormElement
+  
+  itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+    event.preventDefault()
+
+    const input = document.getElementById("newItem") as HTMLInputElement
+    const newEntryText: string = input.value.trim()
+    if(!newEntryText.length) return 
+
+    const itemId:
+
+  })
+  const clearItems = document.getElementById("clearItemsButton") as HTMLButtonElement
+
+  clearItems.addEventListener('click', (): void => {
+    fullList.clearList()
+    templates.clear()
+  })
+
+  fullList.load()
+  templates.render(fullList)
+
+}
+
+document.addEventListener("DOMContentLoaded", initApp)
+
+
+
