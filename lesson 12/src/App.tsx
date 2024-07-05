c146613e8d3c93b729f40c1bac676725e596d9d9
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-05 21:19:16 +0530

    app

--- lesson 12/src/App.tsx
+++ lesson 12/src/App.tsx
@@ -1,7 +1,20 @@
 import Heading from "./components/Heading"
+import {Section} from "./components/Section"
+import Counter from "./components/Counter.tsx"
+
+import { useState } from 'react'
 
 function App() {
-  <Heading title= {"hello"} />
+  const [count, setCount] = useState<number>(1)
+
+  return(
+    <>
+      <Heading title={"HELLO"} />
+      <Section title={"Different Title"}> This is my Section. </Section>
+      <Counter setCount = {setCount}> Count is {count} </Counter>
+    </>
+  )
+  
 }
 
 export default App
