f00553159beeb19ee06a1e9af1e0b4b7959d4b56
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-05 23:27:31 +0530

    app

--- lesson 12/src/App.tsx
+++ lesson 12/src/App.tsx
@@ -1,6 +1,7 @@
 import Heading from "./components/Heading"
 import {Section} from "./components/Section"
 import Counter from "./components/Counter.tsx"
+import List from "./components/List.tsx"
 
 import { useState } from 'react'
 
@@ -12,6 +13,10 @@ function App() {
       <Heading title={"HELLO"} />
       <Section title={"Different Title"}> This is my Section. </Section>
       <Counter setCount = {setCount}> Count is {count} </Counter>
+      <List items = {["coffee", "Tacos", "Code", "heloo"]} render = 
+          {(item: string) => <span className="gold">{item}</span>}>
+
+      </List>
     </>
   )
   
