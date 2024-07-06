624d18ad817982f460bad5ddddd832c48ef79d6c
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-06 11:12:44 +0530

    correction

--- lesson 14 React_useReducer_TS/src/Counter.tsx
+++ lesson 14 React_useReducer_TS/src/Counter.tsx
@@ -22,7 +22,7 @@ initState => {
         case REDUCER_ACTION_TYPE.DECREMENT:
             return {...state, count: state.count - 1}
         case REDUCER_ACTION_TYPE.NEW_INPUT:
-            return {...state, text: action.payload}
+            return {...state, text: action.payload ?? ''}
         default: 
             throw new Error()
     }
