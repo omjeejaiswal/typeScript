85a2da07187ac5aad8e717792aa1f11fdeb417e4
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-07 00:49:22 +0530

    App

--- project - 2/src/App.tsx
+++ project - 2/src/App.tsx
@@ -1,14 +1,25 @@
+import Header from "./components/header"
+import Footer from "./components/footer"
+import Cart from "./components/cart"
+import ProductList from "./components/productList"
+import { useState } from "react"
 
 
 function App() {
 
-  return (
-    <>
-      <div className="App">
+  const [viewCart, setViewCart] = useState<boolean>(false)
 
-      </div>
+  const pageContent = viewCart ? <Cart />: <ProductList/>
+
+  const content = (
+    <>
+      <Header viewCart = {viewCart} setViewCart = {setViewCart}/>
+      {pageContent}
+      <Footer viewCart = {viewCart}/>
     </>
   )
+
+  return content
 }
 
 export default App
