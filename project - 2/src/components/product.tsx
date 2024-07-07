8df495151a58c1901814635316dffa1387c9d83b
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-07 11:22:19 +0530

    product

--- project - 2/src/components/product.tsx
+++ project - 2/src/components/product.tsx
@@ -1,5 +1,39 @@
-const Product = () => {
+import { ProductType } from "../context/ProductsProvider"
+import { ReducerActionType, ReducerAction } from "../context/CartProvider"
+import { ReactElement } from "react"
 
+type PropsType = {
+    product: ProductType,
+    dispatch: React.Dispatch<ReducerAction>,
+    REDUCER_ACTIONS: ReducerActionType,
+    inCart: boolean,
+}
+
+const Product = ({product, dispatch, REDUCER_ACTIONS, inCart}: PropsType): ReactElement => {
+
+    // const img: string = require(`../images/${product.sku}.jpg`)
+
+    const img: string = new URL(`../images/${product.sku}.jpg`, 
+        import.meta.url).href 
+    console.log(img)
+
+    const onAddToCart = () => dispatch({type: REDUCER_ACTIONS.ADD, payload: { ...product, qty: 1 }})
+
+    const itemInCart = inCart ? 'item in cart: ✔' : null
+
+    const content = 
+        <article className= "product" >
+            <h3>{product.name}</h3>
+            <img src="img" alt={product.name}
+            className="product__img" />
+            <p> {new Intl.NumberFormat('en-US', {style: 
+                'currency', currency: 'USD'}). format(product.price)}
+            {itemInCart}</p>
+            <button onClick={onAddToCart}> Add To Cart </button>
+        </article>
+
+
+    return content
 }
 
 export default Product
