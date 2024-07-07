d61fdff9818b907fa196f0fa589cd955bd35a675
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-07 20:48:36 +0530

    product

--- project - 2/src/components/productList.tsx
+++ project - 2/src/components/productList.tsx
@@ -2,7 +2,7 @@ import useCart from "../hooks/useCart"
 import useProducts from "../hooks/useProducts"
 import { UseProductsContextType } from "../context/ProductsProvider"
 import { ReactElement } from "react"
-import Product from "./product"
+import Product from "./Product"
 
 const ProductList = () => {
     const { dispatch, REDUCER_ACTIONS, cart } = useCart()
