393907ea00dadea7a46fd28612fae9ae243a04c4
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-07 11:22:02 +0530

    cart

--- project - 2/src/components/cart.tsx
+++ project - 2/src/components/cart.tsx
@@ -1,7 +1,20 @@
+import useCart from "../hooks/useCart"
+import { useState } from "react"
+import CartLineItem from "./CartLineItem"
+
+
 const Cart = () => {
-    return (
-        <div>Cart</div>
-    )
+
+    const [confirm, setConfirm] = useState<boolean>(false)
+    const {dispatch, REDUCER_ACTIONS, totalItems, TotalPrice, cart} = useCart()
+    const onSumbitOrder = () => {
+        dispatch({type: REDUCER_ACTIONS.SUBMIT})
+        setConfirm(true)
+    }
+
+    const pageContent = confirm
+
+    return pageContent
 }
 
 export default Cart
