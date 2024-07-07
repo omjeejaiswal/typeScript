c209044e46f12d21ffa3c0553a7b613bffd6da94
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-07 20:47:20 +0530

    App.tsx

--- project - 2/src/App.tsx
+++ project - 2/src/App.tsx
@@ -1,21 +1,19 @@
-import Header from "./components/header"
-import Footer from "./components/footer"
-import Cart from "./components/cart"
-import ProductList from "./components/productList"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Cart from "./components/Cart"
+import ProductList from "./components/ProductList"
 import { useState } from "react"
 
-
 function App() {
-
   const [viewCart, setViewCart] = useState<boolean>(false)
 
-  const pageContent = viewCart ? <Cart />: <ProductList/>
+  const pageContent = viewCart ? <Cart /> : <ProductList />
 
   const content = (
     <>
-      <Header viewCart = {viewCart} setViewCart = {setViewCart}/>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
       {pageContent}
-      <Footer viewCart = {viewCart}/>
+      <Footer viewCart={viewCart} />
     </>
   )
 
