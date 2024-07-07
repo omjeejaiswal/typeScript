bc1e7e8f82fb2c13863c40fc289f937d2a30807b
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-07 20:48:05 +0530

    header

--- project - 2/src/components/header.tsx
+++ project - 2/src/components/header.tsx
@@ -6,27 +6,22 @@ type PropsType = {
     setViewCart: React.Dispatch<React.SetStateAction<boolean>>,
 }
 
-
-const Header = ({viewCart, setViewCart}: PropsType) => {
-
-    const {totalItems, totalPrice} = useCart()
+const Header = ({ viewCart, setViewCart }: PropsType) => {
+    const { totalItems, totalPrice } = useCart()
 
     const content = (
         <header className="header">
             <div className="header__title-bar">
-                <h1>
-                    Acme Co.
-                </h1>
+                <h1>Acme Co.</h1>
                 <div className="header__price-box">
-                    <p>Total Items: {totalItems} </p>
-                    <p>Total Prices: {totalPrice} </p>
+                    <p>Total Items: {totalItems}</p>
+                    <p>Total Price: {totalPrice}</p>
                 </div>
             </div>
-            <Nav viewCart = {viewCart} setViewCart={setViewCart} />
+            <Nav viewCart={viewCart} setViewCart={setViewCart} />
         </header>
     )
-    
+
     return content
 }
-
 export default Header
