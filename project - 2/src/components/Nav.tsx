b64cc494169b04f2a7ded307a9e117fa778cc844
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-07 20:48:15 +0530

    Nav

--- project - 2/src/components/Nav.tsx
+++ project - 2/src/components/Nav.tsx
@@ -1,13 +1,12 @@
-
-
 type PropsType = {
     viewCart: boolean,
     setViewCart: React.Dispatch<React.SetStateAction<boolean>>,
 }
 
-const Nav = ({viewCart, setViewCart}: PropsType) => {
+const Nav = ({ viewCart, setViewCart }: PropsType) => {
+
     const button = viewCart
-        ? <button onClick={() => setViewCart(false)}>View Product</button>
+        ? <button onClick={() => setViewCart(false)}>View Products</button>
         : <button onClick={() => setViewCart(true)}>View Cart</button>
 
     const content = (
@@ -17,9 +16,5 @@ const Nav = ({viewCart, setViewCart}: PropsType) => {
     )
 
     return content
-} 
-
-   
- 
-
+}
 export default Nav
